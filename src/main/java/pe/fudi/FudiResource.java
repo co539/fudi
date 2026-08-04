c9c86e5628c8c026bfbbffa571a75664129debe1
co359 <github@rexrexrex.com>
2026-08-04 10:50:36 -0500

    style: reorder

--- src/main/java/pe/fudi/FudiResource.java
+++ src/main/java/pe/fudi/FudiResource.java
@@ -6,11 +6,11 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 @Path( "/hello")
-@Produces({MediaType.TEXT_PLAIN})
 
 public class FudiResource {
 
     @GET
+    @Produces({MediaType.TEXT_PLAIN})
     public String hello() {
         return "Hello World";
     }
