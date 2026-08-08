91c8a756c53ad4cc4f00ca76a21c8751bf25e2aa
co359 <github@rexrexrex.com>
2026-08-08 12:25:06 -0500

    feat: add User repository implementation

---
+++ src/main/java/pe/fudi/iam/infrastructure/persistence/jpa/adapters/UserRepositoryImpl.java
@@ -0,0 +1,57 @@
+package pe.fudi.iam.infrastructure.persistence.jpa.adapters;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import pe.fudi.iam.domain.model.aggregates.User;
+import pe.fudi.iam.domain.model.valueobjects.Email;
+import pe.fudi.iam.domain.model.valueobjects.Username;
+import pe.fudi.iam.domain.repositories.UserRepository;
+import pe.fudi.iam.infrastructure.persistence.jpa.assemblers.UserPersistenceAssembler;
+import pe.fudi.iam.infrastructure.persistence.jpa.repositories.UserPersistenceRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@ApplicationScoped
+public class UserRepositoryImpl implements UserRepository {
+
+    private final UserPersistenceRepository userPersistenceRepository;
+
+    public UserRepositoryImpl(UserPersistenceRepository userPersistenceRepository) {
+        this.userPersistenceRepository = userPersistenceRepository;
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return userPersistenceRepository.findByIdOptional(id)
+                .map(UserPersistenceAssembler::toDomainFromPersistence);
+    }
+
+    @Override
+    public Optional<User> findByUsername(Username username) {
+        return userPersistenceRepository.findByUsername(username.value())
+                .map(UserPersistenceAssembler::toDomainFromPersistence);
+    }
+
+    @Override
+    public Optional<User> findByEmail(Email email) {
+        return userPersistenceRepository.findByEmail(email.value())
+                .map(UserPersistenceAssembler::toDomainFromPersistence);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userPersistenceRepository.listAll().stream()
+                .map(UserPersistenceAssembler::toDomainFromPersistence)
+                .toList();
+    }
+
+    @Override
+    public boolean existsByUsername(Username username) {
+        return userPersistenceRepository.existsByUsername(username.value());
+    }
+
+    @Override
+    public boolean existsByEmail(Email email) {
+        return userPersistenceRepository.existsByEmail(email.value());
+    }
+}
