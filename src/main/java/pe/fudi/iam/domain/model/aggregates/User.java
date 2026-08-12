c475cbdd23d2815f69dda09f25519093d05d637b
co359 <github@rexrexrex.com>
2026-08-12 10:22:08 -0500

    feat: add User constructor methods and new attributes

--- src/main/java/pe/fudi/iam/domain/model/aggregates/User.java
+++ src/main/java/pe/fudi/iam/domain/model/aggregates/User.java
@@ -2,8 +2,13 @@ package pe.fudi.iam.domain.model.aggregates;
 
 import lombok.Getter;
 import lombok.Setter;
+import pe.fudi.iam.domain.model.entities.Role;
 import pe.fudi.iam.domain.model.valueobjects.Email;
 import pe.fudi.iam.domain.model.valueobjects.Username;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Getter
 public class User {
@@ -16,5 +21,39 @@ public class User {
 
     @Setter
     private Email email;
+
+    @Setter
+    private String password;
+
+    @Setter
+    private Set<Role> roles;
+
+    public User() {
+        this.roles = new HashSet<>();
+    }
+
+    public User(Username username, Email email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.roles = new HashSet<>();
+    }
+
+    public User(Username username, Email email, String password, List<Role> roles) {
+        this(username, email, password);
+        addRoles(roles);
+
+    }
+
+    public User addRole(Role role) {
+        this.roles.add(role);
+        return this;
+    }
+
+    public User addRoles(List<Role> roles) {
+        var validatedRoleSet = Role.validateRoleSet(roles);
+        this.roles.addAll(validatedRoleSet);
+        return this;
+    }
 
 }
