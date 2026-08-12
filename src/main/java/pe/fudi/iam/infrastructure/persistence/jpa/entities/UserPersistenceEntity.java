8b7c52248ca523574f439d03044fd1e1d40f9658
co359 <github@rexrexrex.com>
2026-08-12 11:06:28 -0500

    feat: add password and roles fields

--- src/main/java/pe/fudi/iam/infrastructure/persistence/jpa/entities/UserPersistenceEntity.java
+++ src/main/java/pe/fudi/iam/infrastructure/persistence/jpa/entities/UserPersistenceEntity.java
@@ -1,13 +1,14 @@
 package pe.fudi.iam.infrastructure.persistence.jpa.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import pe.fudi.iam.domain.model.valueobjects.Email;
 import pe.fudi.iam.domain.model.valueobjects.Username;
 import pe.fudi.shared.infrastructure.persistence.jpa.entities.AuditableAbstractPersistenceEntity;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "users")
@@ -20,4 +21,13 @@ public class UserPersistenceEntity extends AuditableAbstractPersistenceEntity {
 
     @Column(name = "email", nullable = false, unique = true, length = 120)
     private Email email;
+
+    @Column(name = "password", nullable = false, length = 120)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<RolePersistenceEntity> roles = new HashSet<>();
 }
