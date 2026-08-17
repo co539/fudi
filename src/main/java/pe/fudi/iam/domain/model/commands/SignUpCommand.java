0613e3d9e1ef6d2ff4e1d96e4bf1cf2d7c064e71
co359 <github@rexrexrex.com>
2026-08-17 14:44:21 -0500

    fix: update SignUpCommand

--- src/main/java/pe/fudi/iam/domain/model/commands/SignUpCommand.java
+++ src/main/java/pe/fudi/iam/domain/model/commands/SignUpCommand.java
@@ -1,4 +1,10 @@
 package pe.fudi.iam.domain.model.commands;
 
-public record SignUpCommand(String username, String email, String password) {
+import pe.fudi.iam.domain.model.entities.Role;
+import pe.fudi.iam.domain.model.valueobjects.Email;
+import pe.fudi.iam.domain.model.valueobjects.Username;
+
+import java.util.List;
+
+public record SignUpCommand(Username username, Email email, String password, List<Role> roles) {
 }
