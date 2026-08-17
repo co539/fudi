5d49e6536399463cb81b210380e42e2ea1f8ef77
co359 <github@rexrexrex.com>
2026-08-17 15:53:10 -0500

    feat: update UserResource and UsersController

--- src/main/java/pe/fudi/iam/interfaces/rest/resources/UserResource.java
+++ src/main/java/pe/fudi/iam/interfaces/rest/resources/UserResource.java
@@ -2,16 +2,24 @@ package pe.fudi.iam.interfaces.rest.resources;
 
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
+import java.util.List;
+
 @Schema(
         name = "UserResponse",
         description = "User information response"
 )
 public record UserResource(
+
         @Schema(description = "User unique identifier", examples = "1")
         Long id,
 
         @Schema(description = "User username", examples = "john.doe")
-        String username
-) {
+        String username,
 
+        @Schema(description = "User email", examples = "john@gmail.com")
+        String email,
+
+        @Schema(description = "Roles assigned to the user", examples = "[\"ROLE_USER\"]")
+        List<String> roles
+) {
 }
--- src/main/java/pe/fudi/iam/interfaces/rest/transform/UserResourceFromEntityAssembler.java
+++ src/main/java/pe/fudi/iam/interfaces/rest/transform/UserResourceFromEntityAssembler.java
@@ -1,6 +1,7 @@
 package pe.fudi.iam.interfaces.rest.transform;
 
 import pe.fudi.iam.domain.model.aggregates.User;
+import pe.fudi.iam.domain.model.entities.Role;
 import pe.fudi.iam.interfaces.rest.resources.UserResource;
 
 public final class UserResourceFromEntityAssembler {
@@ -10,7 +11,10 @@ public final class UserResourceFromEntityAssembler {
     }
 
     public static UserResource toResourceFromEntity(User user) {
-        return new UserResource(user.getId(), user.getUsername().value());
+        return new UserResource(
+                user.getId(),
+                user.getUsername().value(),
+                user.getEmail().value(),
+                user.getRoles().stream().map(Role::getStringName).toList());
     }
-
 }
